a0dc04982faf4f59725e1299a3e00b718f3bcecd
Fabio Tavarez <fabiotavarez@pursuit.org>
2023-08-25 09:21:37 -0400

    added deploy URL

--- src/components/API/Axios.js
+++ src/components/API/Axios.js
@@ -4,7 +4,7 @@ const AxiosInstance = axios.create({
   baseURL:
     process.env.NODE_ENV === "development"
       ? "http://localhost:3006"
-      : "DEPLOYED_URL",
+      : "https://portfolio-1cv1.onrender.com",
   timeout: 50000,
 });
 
